e9d624ee55d181160242c230c30b721ab2838bd1
shivank singh <150072306+Sky07light@users.noreply.github.com>
2025-04-15 00:39:46 +0530

    grtting basic info

--- main.py
+++ main.py
@@ -2,3 +2,21 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+
+sns.set(style='whitegrid')
+plt.style.use('seaborn-v0_8-muted')
+
+# Load Dataset
+df = pd.read_csv(r"D:\\PythonPr\\netflix_titles.csv")
+
+# Head of dataset
+print("First 5 Rows:")
+print(df.head())
+
+# Basic info
+print("Info:")
+print(df.info())
+
+# Summary stats
+print("Description of numerical columns:")
+print(df.describe())
